1f9ae3a8b8dcaf74557890681f7d6731b4a677d8
Htet Nandar <htetnandar.282198@gmail.com>
2026-04-10 14:25:23 +0800

    initial commit

---
+++ src/main/java/nus_iss/LAPS/NusIssLapsApplication.java
@@ -0,0 +1,13 @@
+package nus_iss.LAPS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NusIssLapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NusIssLapsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nus_iss/LAPS/NusIssLapsApplicationTests.java
@@ -0,0 +1,13 @@
+package nus_iss.LAPS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NusIssLapsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

bcbb0d9e0f54f60a1a01ddaf2f5f982e1b69b281
Junior <199896254+sailongjunior89@users.noreply.github.com>
2026-04-13 18:08:18 +0800

    uncomment error on local

--- src/main/java/nus_iss/LAPS/model/Employee.java
+++ src/main/java/nus_iss/LAPS/model/Employee.java
@@ -24,6 +24,11 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
+/**
+	* Author: Junior
+ 	* Created on: 13/04/2026
+**/
+
 @Entity
 @Table(name = "employees")
 public class Employee {
@@ -80,10 +85,10 @@ public class Employee {
 	
 	@OneToOne(cascade=CascadeType.ALL, fetch=FetchType.LAZY)
 	@JoinColumn(name="user_id", nullable=false, unique=true)
-	//private User user;
+	private User user;
 	
 	@OneToMany(mappedBy = "employees", cascade=CascadeType.ALL, fetch=FetchType.LAZY)
-	//private List<LeaveBalance> LeaveBalances = new ArrayList<>();
+	private List<LeaveBalance> LeaveBalances = new ArrayList<>();
 	
 	//Self-referenced
 	@ManyToOne(cascade=CascadeType.ALL, fetch=FetchType.LAZY)
@@ -197,22 +202,22 @@ public class Employee {
 		this.updatedWhen = updatedWhen;
 	}
 
-	/*public User getUser() {
+	public User getUser() {
 		return user;
 	}
 
 	public void setUser(User user) {
 		this.user = user;
-	}*/
+	}
 
-	/*public List<LeaveBalance> getLeaveBalances() {
+	public List<LeaveBalance> getLeaveBalances() {
 		return LeaveBalances;
 	}
 
 	public void setLeaveBalances(List<LeaveBalance> leaveBalances) {
 		LeaveBalances = leaveBalances;
 	}
-	 */
+	 
 	public Employee getSupervisor() {
 		return supervisor;
 	}
@@ -235,7 +240,7 @@ public class Employee {
 			String designation,
 			@Min(value = 2020, message = "hire date should not be lesser than 2020") @Max(value = 2099, message = "hire date must be 2099 or earlier") LocalDate hire_date,
 			EmployeeStatus employeeStatus, String createdBy, LocalDateTime createdWhen, String updatedBy,
-			LocalDateTime updatedWhen, /*User user, List<LeaveBalance> leaveBalances,*/ Employee supervisor,
+			LocalDateTime updatedWhen, User user, List<LeaveBalance> leaveBalances, Employee supervisor,
 			List<Employee> subordinates) {
 		super();
 		this.emp_id = emp_id;
@@ -251,8 +256,8 @@ public class Employee {
 		this.createdWhen = createdWhen;
 		this.updatedBy = updatedBy;
 		this.updatedWhen = updatedWhen;
-		//this.user = user;
-		//LeaveBalances = leaveBalances;
+		this.user = user;
+		this.LeaveBalances = leaveBalances;
 		this.supervisor = supervisor;
 		this.subordinates = subordinates;
 	}
@@ -266,17 +271,7 @@ public class Employee {
 				+ email + ", phoneNumber=" + phoneNumber + ", department=" + department + ", designation=" + designation
 				+ ", hire_date=" + hire_date + ", employeeStatus=" + employeeStatus + ", createdBy=" + createdBy
 				+ ", createdWhen=" + createdWhen + ", updatedBy=" + updatedBy + ", updatedWhen=" + updatedWhen
-				+ /*", user=" + user + ", LeaveBalances=" + LeaveBalances + */", supervisor=" + supervisor
+				+ ", user=" + user + ", LeaveBalances=" + LeaveBalances + ", supervisor=" + supervisor
 				+ ", subordinates=" + subordinates + "]";
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 }
